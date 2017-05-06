59c684f98675f104ada0c8e7762fa8158055f622
drklee3 <drklee3@users.noreply.github.com>
2017-05-05 17:26:43 -0700

    clean up

--- ApliaTotalGrade.js
+++ ApliaTotalGrade.js
@@ -18,35 +18,38 @@
     var weights = document.getElementsByClassName("group-weight");
     var grades = [];
 
+    /* Calculate Individual Totals
+    ----------------------------------------------------------------*/
     for (var i = 0; i < tables.length; i++) {
         var scores = tables[i].getElementsByClassName("score-both ");
         // add up points
         for (var j = 0; j < scores.length; j++) {
             if (scores[j].textContent.includes("/")) {
-                console.log(scores[j]);
                 var score_array = scores[j].textContent.split("/");
+
                 numerator += parseFloat(score_array[0]);
                 denominator += parseFloat(score_array[1]);
             }
         }
         // new row of total data
-        var new_row = "<tr><td class='date'></td><td class='name'>Total</td><td class='score-both '>";
-        new_row += numerator + "/" + denominator;
-        new_row += "</td><td class='score-both '>";
-        new_row += ((numerator / denominator) * 100).toFixed(2);
-        new_row += "%</td></tr>";
-        // save data
+        var new_row = "<tr><td class='date'></td><td class='name'>Total</td><td class='score-both '>" +
+            numerator + "/" + denominator +
+            "</td><td class='score-both '>" +
+            ((numerator / denominator) * 100).toFixed(2) +
+            "%</td></tr>";
+        // save data & add data to html
         grades.push(numerator / denominator);
-        // add data to html
         tables[i].firstChild.insertAdjacentHTML("beforeend", new_row);
         // reset values
         numerator = 0;
         denominator = 0;
     }
 
+    /* Calculate Overall Total
+    ----------------------------------------------------------------*/
     var overall_grade = 0;
     var weight_total = 0;   // weighted %s added (in case some dont have grades yet)
-
+    // get weights and add them up
     for (var i = 0; i < weights.length; i++) {
         var weight = weights[i].textContent.match(/\d+% ?/);
         weight = parseInt(weight);
@@ -58,10 +61,9 @@
     }
 
     overall_grade /= weight_total;
-
-    var overall_grade_str = "<div class='right'><h2 class='group'>Total Grade: <span class='group-weight'>";
-    overall_grade_str += (overall_grade * 100).toFixed(2) + "%</span></h2></div>";
-
+    var overall_grade_str = "<div class='right'><h2 class='group'>Total Grade: <span class='group-weight'>" +
+        (overall_grade * 100).toFixed(2) + "%</span></h2></div>";
+    // add data to html
     var header = document.getElementsByClassName("group-info")[0];
     header.insertAdjacentHTML("beforeend", overall_grade_str);
 })();
